ce42ddd4147c5e1733175c7111b726e9782800a9
jadg13 <jose.duran@uamv.edu.ni>
2025-05-08 10:44:20 -0600

    Seguimiento de formacion docente

---
+++ database/migrations/2025_05_07_170845_create_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('course_id');
+            $table->foreignId('course_id')->constrained('courses')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id'); //Profesor
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+            $table->date('course_date_start');
+            $table->time('course_time_start');
+            $table->date('course_date_end');
+            $table->time('course_time_end');
+            $table->string('course_classroom')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schedules');
+    }
+};
